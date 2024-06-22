053a51a4616d77d6a62cbfa54e8f7d801cdf0689
Gussriquena <gustavo_riquena1@outlook.com>
2024-06-22 14:44:43 -0300

    feat (General): project setup

---
+++ src/main/java/com/service/booking/rental/platform/ServiceBookingRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.service.booking.rental.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookingRentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookingRentalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/booking/rental/platform/ServiceBookingRentalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.booking.rental.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookingRentalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
